0e7f26bed1934b1bf54470c0cdaad40ac0562715
Sikuolakazeem <sikuola16@gmail.com>
2026-03-28 23:13:51 +0100

    fix loading spinner

--- src/ui/SpinnerFull.jsx
+++ src/ui/SpinnerFull.jsx
@@ -1,6 +1,6 @@
 function SpinnerFull() {
   return (
-    <main className="mt-50 flex items-center justify-center">
+    <main className="flex h-screen items-center justify-center">
       <span className="spinnerFull z-40"></span>
     </main>
   );
